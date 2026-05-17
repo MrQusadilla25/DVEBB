0c104ff767243dca65c089af864552c50925344c
MrQusadilla25 <ikertyph@gmail.com>
2026-05-17 02:46:52 +0000

    v11

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// phpBB 3.3.x auto-generated configuration file
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'mysql.railway.internal';
+$dbport = '3306';
+$dbname = 'railway';
+$dbuser = 'root';
+$dbpasswd = 'IHSvJPjdwOjBIySQVtfZmJStzSvjhCfG';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
